865e0b00df790071def7dd72686e3027f6fec686
Satyam Chaurasia <satyamchaurasia2011@gmail.com>
2022-08-19 22:55:48 +0530

    update

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -14,6 +14,7 @@ const {
 } = require("../controllers/user");
 
 const memoStorage = multer.memoryStorage();
+
 const upload = multer({ memoStorage });
 
 //for registering users
--- server.js
+++ server.js
@@ -35,23 +35,6 @@ app.use(require("./routes/userRoute"));
 app.use(require("./routes/locationRoute"));
 app.use(require("./routes/requestsRoute"));
 let db = fireAdmin.firestore();
-// app.post("/", async (req, res) => {
-//   const email = "sayamc@gmail.com";
-//   const snapshot = await db
-//     .collection("users")
-//     .where("email", "==", email)
-//     .get();
-//   if (!snapshot.empty) return res.json({ data: "exists" });
-//   await db.collection("users").doc().set({
-//     fullName: "Satyam",
-//     mobile: 2324353,
-//     email: "sayamc@gmail.com",
-//     password: "djsdgjsgjdhkdksdha",
-//   });
-
-//   //console.log(result);
-//   res.json({ data: "send" });
-// });
 
 const PORT = process.env.PORT || "8000";
 
