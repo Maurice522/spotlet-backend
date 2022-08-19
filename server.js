f970cae6d1d214a0ed77337fe277f06e8ae6a949
Satyam Chaurasia <satyamchaurasia2011@gmail.com>
2022-08-20 00:08:22 +0530

    update

--- server.js
+++ server.js
@@ -2,32 +2,10 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const fireAdmin = require("firebase-admin");
-const {
-  TYPE,
-  FIREBASE_PROJECT_ID,
-  PRIVATE_KEY_ID,
-  PRIVATE_KEY,
-  CLIENT_EMAIL,
-  CLIENT_ID,
-  AUTH_URI,
-  TOKEN_URI,
-  AUTH_PROVIDER_X509_CERT_URL,
-  CLIENT_X509_CERT_URL,
-} = require("./config/key");
+const serviceAccount = require("./serviceKey.json");
 
 fireAdmin.initializeApp({
-  credential: fireAdmin.credential.cert({
-    type: TYPE,
-    project_id: FIREBASE_PROJECT_ID,
-    private_key_id: PRIVATE_KEY_ID,
-    private_key: PRIVATE_KEY,
-    client_email: CLIENT_EMAIL,
-    client_id: CLIENT_ID,
-    auth_uri: AUTH_URI,
-    token_uri: TOKEN_URI,
-    auth_provider_x509_cert_url: AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: CLIENT_X509_CERT_URL,
-  }),
+  credential: fireAdmin.credential.cert(serviceAccount),
 });
 app.use(express.json());
 app.use(cors());
