864e1e8388047f1139173ed0de78557d31decf19
Satyam Chaurasia <satyamchaurasia2011@gmail.com>
2022-09-07 10:42:34 +0530

    listing updates

--- controllers/location.js
+++ controllers/location.js
@@ -3,39 +3,109 @@
 const fireAdmin = require("firebase-admin");
 const db = fireAdmin.firestore();
 
+//Random generated Id
+const generateId = () => {
+  let code = "";
+  const characters = "0123456789";
+  for (let i = 0; i < 5; i++)
+    code +=
+      characters[Math.floor(Math.random() * characters.length)];
+  return code;
+}
+
+const tempLocation = async(req, res) => {
+  try {
+    //create a unique location Id
+    const {location_id, data, name} = req.body;
+    if(!location_id){
+      const preName = name.replace(/\s/g,'').toLowerCase();
+      let locationId = preName.substr(0,4) + generateId();
+      let loc = await db.collection("location").doc(locationId).get();
+      while(loc.exists){
+        locationId = "";
+        locationId = preName.substr(4) + generateId();
+        loc = await db.collection("location").doc(locationId).get();
+      }
+      await db.collection("templocation").doc(locationId).set(data);
+      return res.send(locationId);
+    }
+    // const snapshot = await db.collection("templocation").doc(location_id).get();
+    // const templocation = snapshot.data();
+    await db.collection("templocation").doc(location_id).update(data);
+   return res.send("updated");
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+}
+
 
 const locationCreate = async (req, res) => {
-    try {
-        const { userid, type, size, desc, address, city, state, country, pincode, amenities, images, features, dos, donts
-            , hourlyrate, fourhrrate, twelhrrate, timings, rules } = req.body;
-        const data = {
-            property_desc: {
-                userid, type,
-                size, desc,
-            },
-            property_address:{
-                address, city,
-                state, country,
-                pincode
-            }, 
-            amenities:amenities,
-            images:images,
-            features:features,
-            dosndonts:{
-                dos,donts
-            },
-            pricing: { hourlyrate, fourhrrate, twelhrrate },
-            timings:timings,
-            rules:rules
-        };
-        await db.collection("location").doc().set(data);
-        return res.send("Location Created");
+  try {
+    const { property_desc, pricing } = req.body.data;
+      const {location_id} = req.body;
+    const { user_id } = property_desc;
 
-    } catch (error) {
-        return res.status(400).send(error.message);
-    }
+    const { film_webseries_ad, tv_series_other, corporate, individual} = pricing;
+    
+    const data = {
+      timestamp: new Date(),
+      ...req.body.data,
+      pricing : {
+        film_webseries_ad : {
+            type : "Film",
+            isPresent : film_webseries_ad.isPresent,
+            hourly_rate : film_webseries_ad.hourly_rate
+        },
+        tv_series_other : {
+          type : "TV",
+          isPresent : tv_series_other.isPresent,
+          hourly_rate : tv_series_other.hourly_rate
+        },
+        corporate : {
+          type : "Corporate",
+          isPresent : corporate.isPresent,
+          hourly_rate : corporate.hourly_rate
+        },
+        individual : {
+          type : "Individual",
+          isPresent : individual.isPresent,
+          hourly_rate : individual.hourly_rate
+        },
+      },
+      verified : false
+    };
+    
+    if (!user_id) return res.status(422).send("Invalid user");
+
+    await db.collection("location").doc(location_id).set(data);
+    await db.collection("templocation").doc(location_id).delete()
+    const snapshot = await db.collection("users").doc(user_id).get();
+    const user = snapshot.data();
+    console.log(user);
+    await db
+      .collection("users")
+      .doc(user_id)
+      .update({ ...user, listedLocations: [...user.listedLocations, {...data, location_id}] });
+    return res.send("Location Created");
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+};
+
+const getAllLocations = async(req, res) => {
+  try {
+    const snapshots = await db.collection("location").get();
+    const locations = snapshots.docs.map(doc => {
+      return { location_id : doc.id, ...doc.data()};
+    })
+    return res.json({locations});
+  } catch (error) {
+    return res.status(400).send(error);
+  }
 }
 
 module.exports = {
-    locationCreate
-  };
+  locationCreate,
+  getAllLocations,
+  tempLocation,
+};
--- routes/locationRoute.js
+++ routes/locationRoute.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-    locationCreate
-} = require("../controllers/location");
+const { locationCreate, getAllLocations, tempLocation } = require("../controllers/location");
+
+//Create a Temporary Location
+router.post("/templocation", tempLocation)
 
 //Creating a Location.
-router.post("/createl", locationCreate);
+router.post("/createlocation", locationCreate);
+
+//Get All Locations
+router.get("/getlocations", getAllLocations);
 
 
 module.exports = router;
-
