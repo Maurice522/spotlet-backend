f8c0ca9b411a24dc6b827191c7dd1f990c72652c
Saket Mundra <saketmundra2707@gmail.com>
2022-09-27 19:43:06 +0530

    Changes

--- routes/contactUsRoute.js
+++ routes/contactUsRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { sendQueriesToGorecce, photoshootRequest } = require("../controllers/contactUs");
+const { sendQueriesToGorecce, photoshootRequest,getContact } = require("../controllers/contactUs");
 const router = express.Router();
 
 //Contact us
 router.post("/contactus", sendQueriesToGorecce);
+
+router.get("/getContact",getContact);
 
 //Photo Graph form
 router.post("/photoshootreq", photoshootRequest);
