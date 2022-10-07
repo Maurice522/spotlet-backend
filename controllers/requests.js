e2035b6b64401163b932c99d53c8e2145c2cbbf7
Saket Mundra <saketmundra2707@gmail.com>
2022-10-07 14:14:00 +0530

    CHANGES

--- controllers/requests.js
+++ controllers/requests.js
@@ -4,6 +4,42 @@ const db = fireAdmin.firestore();
 const { EMAIL_FROM, SENDGRID_API, EMAIL } = require("../config/key");
 const sgMail = require("@sendgrid/mail");
 
+
+const deleteUser = async (req, res) => {
+  try {
+      // const data=await db.collection("users").doc(req.params.id).get(email);
+      // const emailData = {
+      //     from: EMAIL_FROM,
+      //     to: "saketmundra2707@gmail.com",
+      //     subject: "Account Deactivated",
+      //     html: ` <p style=text-align:center;>Your Account has been Deactivated</p>
+      //           <hr />`,
+      //   };
+      // await sgMail.send(emailData);
+      await db.collection("users").doc(req.params.id).delete();
+      await db.collection("requests").doc(req.params.id).delete();
+      return res.status(200).send("User Deleted");
+  } catch (error) {
+      return res.status(400).json(error);
+  }
+}
+const rejectdeac = async (req, res) => {
+  try {
+      await db.collection("requests").doc(req.params.id).delete();
+      return res.status(200).send("Request Rejected");
+  } catch (error) {
+      return res.status(400).json(error);
+
+  }
+}
+const deleteReq = async (req, res) => {
+  try {
+      const data = await db.collection("requests").get();
+      res.send(data.docs.map(doc => doc.data()));
+  } catch (error) {
+      return res.status(400).json(error);
+  }
+}
 const deleterequests = async (req, res) => {
   try {
     console.log("del");
@@ -33,7 +69,9 @@ const locationrequests = async (req, res) => {
   }
 };
 
-module.exports = {
+module.exports = {deleteUser,
+  rejectdeac,
+  deleteReq,
   deleterequests,
   locationrequests
 };
--- routes/requestsRoute.js
+++ routes/requestsRoute.js
@@ -2,13 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    deleterequests,locationrequests
+    deleterequests,locationrequests,deleteUser,rejectdeac,deleteReq
 } = require("../controllers/requests");
 
 
 router.post("/delreq/:id",deleterequests);
-
 router.get("/locreqs",locationrequests);
+router.delete("/delete/:id", deleteUser);
+router.delete("/rejectdeac/:id",rejectdeac);
+router.get("/deletereq", deleteReq);
 
 
 
