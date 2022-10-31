bd8708c7f25b00f90ba9c83363ff1a1e2a5effee
ijpkaushik <jaiprakash02082001@gmail.com>
2022-10-31 17:48:51 +0530

    images issue resolved

--- config/dev.js
+++ config/dev.js
@@ -3,4 +3,11 @@ module.exports = {
   SENDGRID_API:
     "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",
   EMAIL_FROM: "satyamchaurasia2011@gmail.com",
+  FIREBASE_API_KEY: "AIzaSyDW2-kYrd4E7aAgOKfiLtZreEVIN5Wehrs",
+  FIREBASE_AUTH_DOMAIN: "gorecce-5a416.firebaseapp.com",
+  FIREBASE_PROJECT_ID: "gorecce-5a416",
+  FIREBASE_STORAGE_BUCKET: "gorecce-5a416.appspot.com",
+  FIREBASE_MESSAGING_SENDER_ID: "613994545135",
+  FIREBASE_APP_ID: "1:613994545135:web:c3b3cff7547c0debc2001a",
+  FIREBASE_MEASUREMENT_ID: "G-RE5GGQM60X"
 };
